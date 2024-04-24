b9f91a487a0e32350420b7c81d1316adc5ff196f
almondragon <almondragon@miners.utep.edu>
2024-04-23 20:52:36 -0600

    created assembly file

---
+++ project/toy/led.s
@@ -0,0 +1,52 @@
+	.arch msp430g2553
+	.data
+green_dim_start:	.byte 4
+red_bright_start:	.byte 1
+green_count:	.byte 0
+red_count:	.byte 0
+twinkle_lights:
+	.byte 1
+	.byte 0
+	.byte 1
+	.byte 0
+	.byte 2
+	.byte 0
+	.byte 2
+	.byte 0
+	.byte 3
+	.byte 0
+	.byte 3
+	.byte 0
+	.byte 2
+	.byte 2
+	.byte 0
+	.byte 1
+	.byte 0
+	.byte 1
+	.byte 0
+	.byte 2
+	.byte 0
+	.byte 2
+	.byte 0
+	.byte 3
+	.byte 0
+	.byte 3
+	.byte 0
+	.byte 1
+	.byte 1
+	.byte 0
+	.byte 2
+	.byte 0
+	.byte 2
+	.byte 0
+	.byte 1
+	.byte 0
+	.byte 2
+	.byte 0
+	.byte 2
+	.byte 0
+	.byte 3
+led_seconds:	.word 0
+counter_led_sec:	.byte 0
+	
+	
